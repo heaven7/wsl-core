9ea5d7786006b8b45724fa08f995b838f1d0a954
Christian Grlica <christian@heavenseven.net>
2015-07-22 02:13:38 +0200

    coffeescript package

--- package.js
+++ package.js
@@ -21,6 +21,7 @@ Package.onUse(function(api) {
             'aldeed:simple-schema',
             'mrt:moment',
             'spiderable',
+            'coffeescript',
             'templating'
         ];
 
